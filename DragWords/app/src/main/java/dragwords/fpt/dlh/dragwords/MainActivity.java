664ad4acdb4b59822a176cd2ca7cf388c3ea71a9
bvloi94 <bvloi94@gmail.com>
2015-08-01 15:32:23 +0700

    test

--- DragWords/app/src/main/java/dragwords/fpt/dlh/dragwords/MainActivity.java
+++ DragWords/app/src/main/java/dragwords/fpt/dlh/dragwords/MainActivity.java
@@ -8,14 +8,13 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//ads
     }
 
     @Override
