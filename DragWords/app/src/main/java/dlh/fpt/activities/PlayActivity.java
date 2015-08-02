c63edc49646eae478ba33810be1d0442aa006c78
DatHT <danielhuynh0601@gmail.com>
2015-08-02 10:12:42 +0700

    change

--- DragWords/app/src/main/java/dlh/fpt/activities/PlayActivity.java
+++ DragWords/app/src/main/java/dlh/fpt/activities/PlayActivity.java
@@ -1,5 +1,7 @@
 package dlh.fpt.activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
@@ -10,6 +12,7 @@ import dlh.fpt.R;
 import dlh.fpt.adapter.ViewPagerAdapter;
 
 public class PlayActivity extends FragmentActivity {
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,6 @@ public class PlayActivity extends FragmentActivity {
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
-
+        sharedPreferences = getSharedPreferences("USER_INFO", Context.MODE_PRIVATE);
     }
 }
